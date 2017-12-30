d390357c0a59942134e4468fce9b1d5e26fb126a
sailor1024 <1663852293@qq.com>
2017-12-30 14:46:23 +0800

    1

---
+++ src/L1/L1_001.java
@@ -0,0 +1,7 @@
+package L1;
+
+public class L1_001 {
+public static void main(String[] args) {
+	System.out.println("Hello World!");
+}
+}
---
+++ src/L1/L1_004.java
@@ -0,0 +1,10 @@
+package L1;
+//C = 5*(F-32)/9
+import java.util.Scanner;
+public class L1_004 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	System.out.println("Celsius = "+(5*(a-32)/9));
+}
+}
