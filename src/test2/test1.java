d414ff3f6c778e9ffc9789b38a1b85f6af2da103
sailor1024 <1663852293@qq.com>
2017-12-30 14:59:36 +0800

    4

--- src/test2/test1.java
+++ src/test2/test1.java
@@ -3,7 +3,7 @@ package test2;
 public class test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("ÄãºÃ£¡£¡£¡");
 		System.out.println("ÄãºÃ£¡£¡£¡");
 	}
